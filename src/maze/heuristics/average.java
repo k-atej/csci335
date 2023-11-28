d39c3747e6711ef163a3b83c3d1607f5eb471e18
k-atej <k.atej@comcast.net>
2023-11-28 12:34:52 -0600

    Lab 3 revision
    new heuristics

--- src/maze/heuristics/average.java
+++ src/maze/heuristics/average.java
@@ -7,16 +7,20 @@ import java.util.function.ToIntFunction;
 
 import static java.lang.Math.sqrt;
 
-public class Pythagorean implements ToIntFunction<MazeExplorer> {
+public class average implements ToIntFunction<MazeExplorer> {
+    // finds the manhattan and euclidean distances to the end, averages them
+    // is this monotonic? I think it is, since man and pyth are individually monotonic
     @Override
     public int applyAsInt(MazeExplorer value) {
+        int pyth = pythag(value);
+        int man = value.getLocation().getManhattanDist(value.getM().getEnd());
+        return (man + pyth) / 2;
+    }
+
+    private int pythag(MazeExplorer value) {
         Pos target = value.getM().getEnd();
         int x_diff = target.getX() - value.getLocation().getX();
         int y_diff = target.getY() - value.getLocation().getY();
         return (int) sqrt(x_diff*x_diff + y_diff*y_diff);
     }
 }
-
-// improvements: pythagorean distance to nearest treasures, like mantotreasure (monotonic)
-//              average of pythagorean and manhattan distances? (non-monotonic)
-//              updated pythoagoreanadd to add up distance to treasure and distance between treasure to end (non-monotonic)
---
+++ src/maze/heuristics/averageToTreasure.java
@@ -0,0 +1,84 @@
+package maze.heuristics;
+
+import core.Pos;
+import maze.core.MazeExplorer;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.ToIntFunction;
+
+import static java.lang.Math.sqrt;
+
+public class averageToTreasure implements ToIntFunction<MazeExplorer> {
+// switch to average of the ToTreasure algorithms
+    // is this monotonic
+    @Override
+    public int applyAsInt(MazeExplorer value) {
+        int man = man(value);
+        int pyth = pyth(value);
+        return man + pyth / 2;
+    }
+
+    public int man(MazeExplorer value) {
+        Set<Pos> treasures = value.getAllTreasureFromMaze();
+        Set<Pos> found = value.getAllTreasureFound();
+
+        Set<Pos> unfound = new HashSet<>();
+
+        for (Pos treasure : treasures) {
+            if (!found.contains(treasure)) {
+                unfound.add(treasure);
+            }
+        }
+
+
+        if (unfound.isEmpty()) {
+            return value.getLocation().getManhattanDist(value.getM().getEnd());
+        }
+
+        int closest = value.getLocation().getManhattanDist(value.getM().getEnd());
+
+        for (Pos treasure : unfound) {
+            int dist = value.getLocation().getManhattanDist(treasure);
+            closest = Math.min(closest, dist);
+        }
+
+        return closest;
+    }
+
+    public int pyth(MazeExplorer value) {
+
+
+        Set<Pos> treasures = value.getAllTreasureFromMaze();
+        Set<Pos> found = value.getAllTreasureFound();
+
+        Set<Pos> unfound = new HashSet<>();
+
+        for (Pos treasure : treasures) {
+            if (!found.contains(treasure)) {
+                unfound.add(treasure);
+            }
+        }
+
+
+        if (unfound.isEmpty()) {
+            return pythag(value, value.getM().getEnd());
+        }
+
+        int closest = pythag(value, value.getM().getEnd());
+
+        for (Pos treasure : unfound) {
+            int dist = pythag(value, treasure);
+            closest = Math.min(closest, dist);
+        }
+
+        return closest;
+    }
+
+
+    private int pythag(MazeExplorer value, Pos target) {
+        int x_diff = target.getX() - value.getLocation().getX();
+        int y_diff = target.getY() - value.getLocation().getY();
+        return (int) sqrt(x_diff*x_diff + y_diff*y_diff);
+    }
+}
